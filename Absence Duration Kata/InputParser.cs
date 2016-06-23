2a22c6f951eb52ebddc0621a983863e6d5835c05
Simon Felix <simonf7070@gmail.com>
2016-06-23 16:28:03 +0100

    Parse from dateRange into input string format

---
+++ Absence Duration Kata/InputParser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Absence_Duration_Kata
+{
+    [TestFixture]
+    public class InputParserTests
+    {
+        [Test]
+        public void ParseDateRange()
+        {
+            var dateRange = new DateRange(DateTime.Today, DateTime.Today.AddDays(5));
+            Assert.That(dateRange.ToInputString(), Is.EqualTo("XXXXX"));
+        }
+    }
+
+    public static class InputParser
+    {
+        public static string ToInputString(this DateRange dateRange)
+        {
+            var numberOfDays = (int)(dateRange.End - dateRange.Start).TotalDays;
+            return new string('X', numberOfDays);
+        }
+    }
+}
